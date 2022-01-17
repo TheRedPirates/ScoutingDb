f6e72d501985a672a71232445eee1699d624f7c1
arielpro2 <37239599+arielpro2@users.noreply.github.com>
2022-01-17 16:18:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import secrets
 import javaobj
 import csv
 import flask
+import os
 from flask import render_template, request
 
 app = flask.Flask(__name__)
@@ -14,10 +15,16 @@ def home():
 @app.route('/register')
 def register():
     teamtoken = secrets.token_urlsafe(16)
-    with open(f"/TeamData/{teamtoken}.csv",'w') as file:
-        file.write("Alias,TeamNumber,GameNumber,b_defence,b_unsure,b_offence,AutoUpper,AutoLower,TeleUpper,TeleLower,Ranking,Notes,C1,C2,C3,C4\n")
     with open("secrets",'a') as secretfile:
         secretfile.write(f"{request.args.get('TeamNumber')}:{teamtoken}")
+        
+    os.chdir('/TeamData')
+   
+    with open(f"{teamtoken}.csv",'w') as file:
+        file.write("Alias,TeamNumber,GameNumber,b_defence,b_unsure,b_offence,AutoUpper,AutoLower,TeleUpper,TeleLower,Ranking,Notes,C1,C2,C3,C4\n")
+    
+    os.chdir('..')
+    
     return f"""
         Successfully registered team,
         Please save and use this token in your login credentials on our mobile app:
@@ -38,10 +45,16 @@ def login():
 def createEntry():
     objectbytes = request.args.get('object')
     pobj = javaobj.loads(bytearray.fromhex(objectbytes))
-    entries = open(f"/TeamData/{pobj.Token}.csv", "a")
+    
+    os.chdir('/TeamData')
+    
+    entries = open(f"{pobj.Token}.csv", "a")
     string = f"{pobj.Alias},{pobj.TeamNumber},{pobj.GameNumber},{pobj.b_defence},{pobj.b_unsure},{pobj.b_offence},{pobj.AutoUpper},{pobj.AutoLower},{pobj.TeleUpper},{pobj.TeleLower},{pobj.Ranking},{pobj.Notes},{pobj.C1},{pobj.C2},{pobj.C3},{pobj.C4}\n"
     entries.write(string)
     entries.close()
+    
+    os.chdir('..')
+    
     return "succsesfully created entry"
 
 app.run(host="0.0.0.0",port=5000)
