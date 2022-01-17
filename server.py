39454beed2c9cb9eac43f2c6b2b94225a8e4b0ff
arielpro2 <37239599+arielpro2@users.noreply.github.com>
2022-01-17 16:00:52 +0200

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def home():
 @app.route('/register')
 def register():
     teamtoken = secrets.token_urlsafe(16)
-    with open(f"{teamtoken}.csv",'w') as file:
+    with open(f"/TeamData/{teamtoken}.csv",'w') as file:
         file.write("Alias,TeamNumber,GameNumber,b_defence,b_unsure,b_offence,AutoUpper,AutoLower,TeleUpper,TeleLower,Ranking,Notes,C1,C2,C3,C4\n")
     with open("secrets",'a') as secretfile:
         secretfile.write(f"{request.args.get('TeamNumber')}:{teamtoken}")
@@ -38,7 +38,7 @@ def login():
 def createEntry():
     objectbytes = request.args.get('object')
     pobj = javaobj.loads(bytearray.fromhex(objectbytes))
-    entries = open(f"{pobj.Token}.csv", "a")
+    entries = open(f"/TeamData/{pobj.Token}.csv", "a")
     string = f"{pobj.Alias},{pobj.TeamNumber},{pobj.GameNumber},{pobj.b_defence},{pobj.b_unsure},{pobj.b_offence},{pobj.AutoUpper},{pobj.AutoLower},{pobj.TeleUpper},{pobj.TeleLower},{pobj.Ranking},{pobj.Notes},{pobj.C1},{pobj.C2},{pobj.C3},{pobj.C4}\n"
     entries.write(string)
     entries.close()
